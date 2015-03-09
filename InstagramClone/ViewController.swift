1feb74a1ffd205310c706ff358a5da46d739fb09
Rommel Rico <rommeltj@gmail.com>
2015-03-09 11:28:29 -0700

    Parse Hello World
    API Keys REDACTED

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -12,7 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Parse.setApplicationId("REDACTED", clientKey: "REDACTED")
+        var score = PFObject(className: "score")
+        score.setObject("Rommel", forKey: "name")
+        score.setObject(23, forKey: "number")
+        score.saveInBackgroundWithBlock { (success, error) -> Void in
+            if(success) {
+                NSLog("We were successful! ObjectId = \(score.objectId)")
+            } else {
+                NSLog("ERROR: \(error)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
